a0fb937740abdd26aebab8cf2d30d837e34a02a0
Dong Van Cong <dongvancong24@gmail.com>
2024-02-27 14:46:14 +0700

    my apppppp

--- server/router/Project.js
+++ server/router/Project.js
@@ -15,13 +15,40 @@ router.get('/project', function(req, res) {
     `;
     db.query(query, function(err, result) {
         if (err) {
-            console.error(err);
+            console.error("Error occurred:", err); // Log lỗi ra console
             res.status(500).send("Lỗi Server Nội Bộ");
         } else {
             res.status(200).json(result);
         }
     });
-  });
+});
+
+var path = require('path');
+module.exports = router;
+var router = require('express')();
+var db = require('./dbConnect');
+
+// Lấy danh sách dự án
+router.get('/project', function(req, res) {
+    const query = `
+      SELECT project.*, projectDetails.*,
+       user.fullName AS leadFullName ,
+       user.picture AS imgUser ,
+      team.teamName AS teamFullName
+      FROM project
+      LEFT JOIN projectDetails ON project.projectID = projectDetails.projectID
+      LEFT JOIN Users AS user ON projectDetails.userID = user.userID
+      LEFT JOIN Team AS team ON projectDetails.teamID = team.teamID
+    `;
+    db.query(query, function(err, result) {
+        if (err) {
+            console.error("Error occurred:", err); // Log lỗi ra console
+            res.status(500).send("Lỗi Server Nội Bộ");
+        } else {
+            res.status(200).json(result);
+        }
+    });
+});
 
 var path = require('path');
 module.exports = router;
